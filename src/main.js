1431b47bad6dcfcac4fa437d5875f8afa89b5295
Mexty <m.harchenko@i.ua>
2024-10-27 12:55:29 +0200

    Added

--- src/main.js
+++ src/main.js
@@ -2,10 +2,18 @@ import { nanoid } from 'nanoid';
 const form = document.querySelector('.header-form');
 const cardsList = document.querySelector('.tasks-list');
 const STORAGE_KEY = 'tasks';
-
+initSt();
 form.addEventListener('submit', formSubmit);
 
-initSt();
+cardsList.addEventListener('click', deleteCard);
+function deleteCard(event) {
+  if (event.target.nodeName !== 'BUTTON') {
+    return;
+  }
+  const id = event.target.dataset.id;
+  deleteTasktoLocaleSt(id);
+  renderCards();
+}
 
 function formSubmit(event) {
   event.preventDefault();
@@ -18,12 +26,12 @@ function formSubmit(event) {
   const newTask = { formName, formText, id: nanoid() };
 
   addTasktoLocaleSt(newTask);
-  const tasksLocaleSt = JSON.parse(localStorage.getItem(STORAGE_KEY));
 
-  renderCards(tasksLocaleSt);
+  renderCards();
 }
 
-function renderCards(tasksLocaleSt) {
+function renderCards() {
+  const tasksLocaleSt = JSON.parse(localStorage.getItem(STORAGE_KEY));
   const cards = tasksLocaleSt
     .map(
       ({ formName, formText, id }) => `<li class="task-list-item">
@@ -41,6 +49,12 @@ function addTasktoLocaleSt(newTask) {
   const arrTasks = JSON.parse(localStorage.getItem(STORAGE_KEY));
   arrTasks.push(newTask);
   localStorage.setItem(STORAGE_KEY, JSON.stringify(arrTasks));
+}
+
+function deleteTasktoLocaleSt(id) {
+  const arrTasks = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  const filterArr = arrTasks.filter(element => element.id !== id);
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(filterArr));
 }
 
 function initSt() {
